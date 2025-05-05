52fb950e094b8ce602d774272464aaaf3ca02a1e
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-05 11:41:47 +0700

    Update Admin Courses

--- course/admin.py
+++ course/admin.py
@@ -3,6 +3,11 @@ from .models import Course
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    # Menampilkan kolom-kolom ini di halaman daftar (list view) admin
     list_display = ["title", "instructor", "created_at", "updated_at"]
+
+    # Menambahkan kolom yang bisa dicari menggunakan search box
     search_fields = ["title", "instructor__username"]
+
+    # Menambahkan filter sidebar berdasarkan kolom tertentu
     list_filter = ["created_at", "updated_at", "instructor"]
