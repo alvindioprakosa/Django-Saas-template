69085ba450ef15b3e0816ee6c7f3f358238aa2e0
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-05 11:41:57 +0700

    Update Models Courses

--- course/models.py
+++ course/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses_taught")
-    subscribers = models.ManyToManyField(User, related_name="courses_subscirbed", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # Otomatis saat dibuat
+    updated_at = models.DateTimeField(auto_now=True)      # Otomatis saat diubah
+    instructor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="courses_taught"  # Untuk mengakses course dari user.instructor
+    )
+    subscribers = models.ManyToManyField(
+        User,
+        related_name="courses_subscribed",  # <-- typo diperbaiki di sini
+        blank=True  # Membolehkan course tanpa subscriber
+    )
     price = models.DecimalField(max_digits=6, decimal_places=2, default=1.99)
 
     def __str__(self):
